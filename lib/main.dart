27924480b6bcdb02f889c62a2dc36addb3218824
liyongjin <yongjin.li1@pactera.com>
2020-03-12 17:31:35 +0800

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.//我是Moore，我现在更改了我的分支
         primarySwatch: Colors.blue,
-      ),
+      ),///我再次更改了我的分支内容
       home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
